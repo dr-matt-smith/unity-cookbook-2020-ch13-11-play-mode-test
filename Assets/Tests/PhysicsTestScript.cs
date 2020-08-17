ee75687850635d1ac5ed833faa7335c661326af0
dr-matt-smith <matt.smith@itb.ie>
2020-08-17 14:27:28 +0100

    added files to project

---
+++ Assets/Tests/PhysicsTestScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+ 
+public class PhysicsTestScript
+{
+    [UnityTest]
+    public IEnumerator GameObject_WithRigidBody_WillBeAffectedByPhysics()
+    {
+        // Arrange
+        var go = new GameObject();
+        go.AddComponent<Rigidbody>();
+        var originalPosition = go.transform.position.y;
+ 
+        // Act
+        yield return new WaitForFixedUpdate();
+ 
+        // Assert
+        Assert.AreNotEqual(originalPosition, go.transform.position.y);
+    }
+} 
